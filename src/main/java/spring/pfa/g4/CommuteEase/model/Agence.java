f269f83bbdd762c96b730e24bc607c5275229f8a
goldenadnane <adnaneabouelmakarim@gmail.com>
2023-04-03 22:56:46 +0000

    Adnane Commit

---
+++ src/main/java/spring/pfa/g4/CommuteEase/model/Agence.java
@@ -0,0 +1,69 @@
+package spring.pfa.g4.CommuteEase.model;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+
+@Entity
+public class Agence {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nom;
+    private String adresse;
+    private Number telephone;
+    private String driver;
+
+/*
+    @OneToMany(mappedBy = "agencebus",fetch=FetchType.LAZY)
+    private List<Bus> bus= new ArrayList<Bus>();
+
+    public List<Bus> getBus() {
+        return bus;
+    }
+
+    public void setBus(List<Bus> bus) {
+        this.bus = bus;
+    }
+*/
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public Number getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(Number telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getDriver() {
+        return driver;
+    }
+
+    public void setDriver(String driver) {
+        this.driver = driver;
+    }
+
+}
